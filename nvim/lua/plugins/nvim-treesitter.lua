3e907db5ba7dbd9d3603bd5bf1d143df64a31db2
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-09-11 18:39:22 +1000

    add treesitter

---
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,7 @@
+-- see https://github.com/LazyVim/LazyVim/blob/a72a84972d85e5bbc6b9d60a0983b37efef21b8a/lua/lazyvim/plugins/treesitter.lua#L7
+
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects", },
+}
