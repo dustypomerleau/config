3b0b83d1c6dadf32e24ee5f4e1c75ec66693e0a5
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-09-11 21:19:40 +1000

    add sort-motion keybindings

--- nvim/lua/plugins/vim-sort-motion.lua
+++ nvim/lua/plugins/vim-sort-motion.lua
@@ -2,5 +2,25 @@ return {
     {
         "christoomey/vim-sort-motion",
         config = function() vim.g.sort_motion_flags = "i" end,
+        keys = {
+            {
+                "gs",
+                function()
+                    require("vim-sort-motion").cmd("SortMotion")
+                end,
+                mode = { "n", },
+                silent = true,
+                noremap = true,
+            },
+            {
+                "gs",
+                function()
+                    require("vim-sort-motion").cmd("SortMotionVisual")
+                end,
+                mode = { "x", },
+                silent = true,
+                noremap = true,
+            },
+        },
     },
 }
