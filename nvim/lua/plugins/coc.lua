3421a1752872ceef631e05999a9bf33598961ca8
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-09-11 21:10:06 +1000

    add coc text objects and documentation function

--- nvim/lua/plugins/coc.lua
+++ nvim/lua/plugins/coc.lua
@@ -1,3 +1,13 @@
+local fn = vim.fn
+
+function ShowDocumentation()
+    if fn.CocAction("hasProvider", "hover") then
+        fn.CocActionAsync("doHover")
+    else
+        fn.feedkeys("K", "in")
+    end
+end
+
 return {
     {
         "neoclide/coc.nvim",
@@ -20,6 +30,13 @@ return {
                 "<leader>v",
                 "<cmd>CocCommand markdown-preview-enhanced.openPreview<cr>",
                 mode = { "n", },
+                noremap = true,
+            },
+            {
+                "<localleader>r",
+                function() require("coc.nvim").cmd("coc-rename") end,
+                mode = { "n", },
+                silent = true,
                 noremap = true,
             },
             {
@@ -50,7 +67,41 @@ return {
                 silent = true,
                 noremap = true,
             },
-
+            {
+                -- test this, not certain it's correct
+                -- for reference: https://github.com/jonwalstedt/dotfiles/blob/4aeb6914948cd354765961738f42f68d1a7083db/config/nvim-coc/lua/plugin/coc.lua#L106
+                "K",
+                function() ShowDocumentation() end,
+                mode = { "n", },
+                silent = true,
+                noremap = true,
+            },
+            -- text objects
+            {
+                "if",
+                function() require("coc.nvim").cmd("coc-funcobj-i") end,
+                mode = { "o", "x", },
+                silent = true,
+            },
+            {
+                "af",
+                function() require("coc.nvim").cmd("coc-funcobj-a") end,
+                mode = { "o", "x", },
+                silent = true,
+            },
+            {
+                "ic",
+                function() require("coc.nvim").cmd("coc-classobj-i") end,
+                mode = { "o", "x", },
+                silent = true,
+            },
+            {
+                "ac",
+                function() require("coc.nvim").cmd("coc-classobj-a") end,
+                mode = { "o", "x", },
+                silent = true,
+            },
+            -- todo: scroll in float windows coc#float#has_scroll()
         },
     },
 }
