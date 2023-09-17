b713d48afd4f727cb3a7a0192a4aa8cae536edd7
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-09-17 18:48:56 +1000

    configure cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -4,15 +4,69 @@
 return {
     "hrsh7th/nvim-cmp",
     config = function()
-        require("mason").setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
+        local cmp = require("nvim-cmp")
+
+        cmp.setup({
+            snippet = {
+                -- snippet engine is required
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
             },
+            window = {
+                -- completion = cmp.config.window.bordered(),
+                -- documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true, }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }),
+            sources = cmp.config.sources(
+                { { name = "luasnip", }, },
+                { { name = "buffer", }, }
+            ),
         })
+
+        -- -- Set configuration for specific filetype.
+        -- cmp.setup.filetype("gitcommit", {
+        --     sources = cmp.config.sources({
+        --         { name = "git", }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+        --     }, {
+        --         { name = "buffer", },
+        --     }),
+        -- })
+
+        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline(
+            { "/", "?", },
+            {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = { { name = "buffer", }, },
+            }
+        )
+
+        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline(":", {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = "path", },
+            }, {
+                { name = "cmdline", },
+            }),
+        })
+
+        -- Set up lspconfig.
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+        lsp.clangd.setup({ capabilities = capabilities, })
+        lsp.lua_ls.setup({ capabilities = capabilities, })
+        lsp.pyright.setup({ capabilities = capabilities, })
+        lsp.rust_analyzer.setup({ capabilities = capabilities, })
+        lsp.svelte.setup({ capabilities = capabilities, })
+        lsp.tailwindcss.setup({ capabilities = capabilities, })
+        lsp.tsserver.setup({ capabilities = capabilities, })
     end,
-    -- config = true, -- https://github.com/hrsh7th/nvim-cmp#recommended-configuration
 }
