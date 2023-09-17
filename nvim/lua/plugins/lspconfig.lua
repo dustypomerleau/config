cab5d957feaae66a8e21c4e086040feb3fd38906
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-09-17 10:19:31 +1000

    add border to LSP info window

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,9 +1,11 @@
+-- default server configs for reference:
+-- https://github.com/neovim/nvim-lspconfig/tree/master/lua/lspconfig/server_configurations
+
 return {
     "neovim/nvim-lspconfig",
     event = "VeryLazy",
     config = function()
         local lsp = require("lspconfig")
-
         lsp.clangd.setup({})
 
         lsp.lua_ls.setup({
@@ -21,5 +23,7 @@ return {
         lsp.svelte.setup({})
         lsp.tailwindcss.setup({})
         lsp.tsserver.setup({})
+
+        require("lspconfig.ui.windows").default_options.border = "rounded"
     end,
 }
