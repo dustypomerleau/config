a10d5e0d7ed61363de6d167b4ba0bba31632d8cf
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-09-20 22:55:55 +1000

    formatting

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -251,36 +251,31 @@ return {
             "s",
             mode = { "n", "o", "x", },
             function() require("flash").jump() end,
-            desc =
-            "Flash",
+            desc = "Flash",
         },
         {
             "S",
             mode = { "n", "o", "x", },
             function() require("flash").treesitter() end,
-            desc =
-            "Flash Treesitter",
+            desc = "Flash Treesitter",
         },
         {
             "r",
             mode = "o",
             function() require("flash").remote() end,
-            desc =
-            "Remote Flash",
+            desc = "Remote Flash",
         },
         {
             "R",
             mode = { "o", "x", },
             function() require("flash").treesitter_search() end,
-            desc =
-            "Treesitter Search",
+            desc = "Treesitter Search",
         },
         {
             "<c-s>",
             mode = { "c", },
             function() require("flash").toggle() end,
-            desc =
-            "Toggle Flash Search",
+            desc = "Toggle Flash Search",
         },
     },
 }
