7fa7ff0a0f7226d40d584f16f109e2fca8b7c2e1
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-09-24 17:30:26 +1000

    remove all conflicts between flash and nvim-surround by switching to oO

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -3,8 +3,22 @@ return {
         "kylechui/nvim-surround",
         event = "VeryLazy",
         config = function()
-            -- avoid a conflict with flash in `o` mode
-            require("nvim-surround").setup({ keymaps = { delete = "do", }, })
+            -- avoid conflicts with flash by changing sS to oO
+            require("nvim-surround").setup({
+                keymaps = {
+                    change = "co",
+                    change_line = "cO",
+                    delete = "do",
+                    insert = "<C-g>o",
+                    insert_line = "<C-g>O",
+                    normal = "yo",
+                    normal_cur = "yoo",
+                    normal_cur_line = "yOO",
+                    normal_line = "yO",
+                    visual = "O",
+                    visual_line = "gO",
+                },
+            })
         end,
     },
 }
