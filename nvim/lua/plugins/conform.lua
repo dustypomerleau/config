df373edb0fbf4b2e96901d1ef4f049c430ab407b
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-10-02 17:35:40 +1100

    notes

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -7,7 +7,7 @@ return {
                 formatters_by_ft = {
                     -- using a sublist will run only the first available formatter
                     javascript = { { "prettierd", "prettier", }, },
-                    lua = {}, -- LSP uses EmmyLua, but it's not exactly clear how this is called.
+                    lua = {}, -- LSP uses EmmyLua, but it's not exactly clear how this is called (https://github.com/CppCXY/EmmyLuaCodeStyle).
                     markdown = { { "prettierd", "prettier", }, },
                     rust = { "rustfmt", },
                     svelte = { { "prettierd", "prettier", }, },
