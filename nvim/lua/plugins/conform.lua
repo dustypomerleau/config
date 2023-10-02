1f9e22d13716b598843d8cb3f81ad596dc3e27aa
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-10-02 17:40:16 +1100

    notes; enable only necessary formatters

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,11 +6,14 @@ return {
             require("conform").setup({
                 formatters_by_ft = {
                     -- using a sublist will run only the first available formatter
+                    -- my impression is that it's best to lean on vim.lsp.buf.format()
+                    -- from init.vim, but if it's not giving the desired result, like
+                    -- typescript, then add it here
                     javascript = { { "prettierd", "prettier", }, },
-                    lua = {}, -- LSP uses EmmyLua, but it's not exactly clear how this is called (https://github.com/CppCXY/EmmyLuaCodeStyle).
+                    -- lua = {}, -- LSP uses EmmyLua, but it's not exactly clear how this is called (https://github.com/CppCXY/EmmyLuaCodeStyle).
                     markdown = { { "prettierd", "prettier", }, },
-                    rust = { "rustfmt", },
-                    svelte = { { "prettierd", "prettier", }, },
+                    -- rust = { "rustfmt", },
+                    -- svelte = { { "prettierd", "prettier", }, },
                     typescript = { { "prettierd", "prettier", }, },
                 },
                 -- setting format_on_save will automatically generate an autocommand like:
