9590ed7e03d5b428789dfa20c92ab02532ebafcd
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-11-12 21:09:46 -0500

    notes

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -4,7 +4,7 @@ function fish_right_prompt
     set_color 7e818b
     echo -n (date '+%Y-%m-%d ') # -n suppresses newline
     set_color blue
-    echo -n (date '+%H:%M:%S') # -n suppresses newline
+    echo -n (date '+%H:%M:%S')
     set_color ff4ee0
     printf '] '
 end
