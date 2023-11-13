1eb7769c8e1a117d79d1286f192b053b091de99b
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-11-12 21:05:48 -0500

    add time and date in fish right prompt

---
+++ fish/functions/fish_right_prompt.fish
@@ -0,0 +1,10 @@
+function fish_right_prompt
+    set_color ff4ee0
+    printf '['
+    set_color blue
+    echo -n (date '+%H:%M:%S ') # -n suppresses newline
+    set_color 7e818b
+    echo -n (date '+%Y-%m-%d') # -n suppresses newline
+    set_color ff4ee0
+    printf '] '
+end
