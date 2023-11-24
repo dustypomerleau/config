5bbef0fa6c83636e3d330081282bba3c67347116
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-11-24 17:16:15 +1100

    formatting

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -6,17 +6,14 @@ return { {
     event = "VeryLazy",
     config = function()
         -- enable inlay hints on attach if the LSP supports them
-        vim.api.nvim_create_autocmd(
-            "LspAttach",
-            {
-                group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-                callback = function(args)
-                    local client = vim.lsp.get_client_by_id(args.data.client_id)
-                    if client.server_capabilities.inlayHintProvider then
-                        vim.lsp.inlay_hint.enable(args.buf, true)
-                    end
-                end,
-            }
-        )
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+            callback = function(args)
+                local client = vim.lsp.get_client_by_id(args.data.client_id)
+                if client.server_capabilities.inlayHintProvider then
+                    vim.lsp.inlay_hint.enable(args.buf, true)
+                end
+            end,
+        })
     end,
 }, }
