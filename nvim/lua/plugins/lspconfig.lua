1672660e7aa82b1bdff2264c30184c4e719bc6d7
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-11-24 20:25:34 +1100

    use `supports_method` rather than `server_capabilities` to test inlay hint support

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -10,7 +10,7 @@ return { {
             group = vim.api.nvim_create_augroup("UserLspConfig", {}),
             callback = function(args)
                 local client = vim.lsp.get_client_by_id(args.data.client_id)
-                if client.server_capabilities.inlayHintProvider then
+                if client.supports_method("textDocument/inlayHint") then
                     vim.lsp.inlay_hint.enable(args.buf, true)
                 end
             end,
