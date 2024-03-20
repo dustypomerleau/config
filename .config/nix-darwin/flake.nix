c785096f4586037d71f8ce6026928c0d57af0331
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-03-20 15:58:31 +1100

    chore(nix): formatting

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -1,4 +1,6 @@
 # https://librephoenix.com/2023-11-02-how-to-manage-your-dotfiles-the-nix-way-with-home-manager
+# https://www.jetpack.io/devbox/docs/guides/using_flakes/
+# https://www.breakds.org/post/flake-part-1-packaging/
 
 {
   description = "DP MBP system flake";
@@ -9,45 +11,67 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [ pkgs.vim ];
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
+          # Auto upgrade nix package and the daemon service.
+          services.nix-daemon.enable = true;
+          # nix.package = pkgs.nix;
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
 
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      programs.fish.enable = true;
+          # Create /etc/zshrc that loads the nix-darwin environment.
+          programs.zsh.enable = true; # default shell on catalina
+          programs.fish.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 4;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "x86_64-darwin";
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "x86_64-darwin";
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#DP-2018-MBP
+      darwinConfigurations."DP-2018-MBP" = nix-darwin.lib.darwinSystem { modules = [ configuration ]; };
+
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."DP-2018-MBP".pkgs;
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#DP-2018-MBP
-    darwinConfigurations."DP-2018-MBP" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."DP-2018-MBP".pkgs;
-  };
 }
+
+# outputs = { self, nixpkgs, fh, .. } @ inputs:
+#     let
+#       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+#       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+#         inherit system;
+#         pkgs = import nixpkgs { inherit system; };
+#       });
+#     in
+#     {
+#       devShells = forEachSupportedSystem ({ pkgs, system }: {
+#         default = pkgs.mkShell {
+#           packages = [
+#             fh.packages.${system}.fh
+#           ];
+#         };
+#       });
+#     };
+# }
