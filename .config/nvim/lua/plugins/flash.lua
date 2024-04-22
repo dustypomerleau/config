cfe77a60b9d38e16975e7c4480460bec7b239d82
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-04-23 09:34:28 +1000

    feat(nvim): enable flash by default on search

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -138,18 +138,18 @@ return {
             modes = {
                 -- -- options used when flash is activated through
                 -- -- a regular search with `/` or `?`
-                -- search = {
-                --     -- when `true`, flash will be activated during regular search by default.
-                --     -- You can always toggle when searching with `require("flash").toggle()`
-                --     enabled = true,
-                --     highlight = { backdrop = false, },
-                --     jump = { history = true, register = true, nohlsearch = true, },
-                --     search = {
-                --         -- `forward` will be automatically set to the search direction
-                --         -- `mode` is always set to `search`
-                --         -- `incremental` is set to `true` when `incsearch` is enabled
-                --     },
-                -- },
+                search = {
+                    -- when `true`, flash will be activated during regular search by default.
+                    -- You can always toggle when searching with `require("flash").toggle()`
+                    enabled = true,
+                    highlight = { backdrop = true, },
+                    jump = { history = true, register = true, nohlsearch = true, },
+                    search = {
+                        -- `forward` will be automatically set to the search direction
+                        -- `mode` is always set to `search`
+                        -- `incremental` is set to `true` when `incsearch` is enabled
+                    },
+                },
                 -- -- options used when flash is activated through
                 -- -- `f`, `F`, `t`, `T`, `;` and `,` motions
                 char = {
