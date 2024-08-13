195b8554970ef49379952bd892269f4349b0a6b2
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-08-13 16:46:17 +1000

    feat(nix): wip: migrate brews

--- nix-darwin/packages.nix
+++ nix-darwin/packages.nix
@@ -11,6 +11,17 @@
     # systemPackages are installed for all users, unlike profile packages
     systemPackages = [
       # todo: add remaining packages
+      pkgs.asciidoctor
+      pkgs.awscli2
+      pkgs.bat
+      pkgs.bottom
+      pkgs.broot
+      pkgs.cloc
+      pkgs.cmake
+      pkgs.curl
+      pkgs.edgedb
+      pkgs.eza
+      pkgs.gawk
       pkgs.nixd
       pkgs.nixfmt-rfc-style
       pkgs.vim
@@ -21,3 +32,79 @@
   fonts.packages = [ ];
 
 }
+
+# packages to migrate from brew:
+# 
+# x asciidoctor
+# x awk
+# x awscli
+# x bat
+# x bottom
+# x broot
+# cabal-install
+# x cloc
+# x cmake
+# x curl
+# difftastic
+# duti
+# x edgedb/tap/edgedb-cli
+# elixir
+# exercism
+# x eza
+# fd
+# ffmpeg@4
+# findutils
+# fontforge
+# fzf
+# gdb
+# gh
+# git
+# git-delta
+# glow
+# gnu-sed
+# grep
+# haskell-stack
+# helix
+# idris
+# idris2
+# jq
+# libpq
+# librsvg
+# libzip
+# luarocks
+# mas
+# mysql
+# nasm
+# neovim
+# node
+# ocrmypdf
+# pandoc
+# parallel
+# pnpm
+# podman
+# postgresql@11
+# postgresql@13
+# postgresql@14
+# python@3.8
+# qmk/qmk/qmk
+# r
+# ripgrep-all
+# rlue/utils/timer
+# rust-parallel
+# rustup
+# showkey
+# taplo
+# tealdeer
+# tere
+# tmux
+# tokei
+# tree
+# typst
+# uutils-coreutils
+# watchman
+# yarn
+# youtube-dl
+# zola
+# zoxide
+# zsh
+# zsh-syntax-highlighting
