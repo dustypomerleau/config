229ca0476f68957a09f4bef7746e99fc6a306786
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-08-15 17:11:31 +1000

    feat(packages): add yazi, google fonts, and use `with`

--- nix-darwin/packages.nix
+++ nix-darwin/packages.nix
@@ -1,82 +1,99 @@
 { pkgs, ... }:
 {
   environment = {
-    shells = [
-      pkgs.zsh
-      pkgs.fish
+    shells = with pkgs; [
+      zsh
+      fish
     ];
 
     # installed for all users, unlike `home.packages`
-    systemPackages = [
+    systemPackages = with pkgs; [
       ## todo: not available from cargo: crates-tui, rimage
-      pkgs.asciidoctor
-      pkgs.awscli2
-      pkgs.bat
-      pkgs.bfg-repo-cleaner
-      pkgs.bottom
-      pkgs.broot # docs: https://dystroy.org/broot/
-      pkgs.cargo-binstall
-      pkgs.cargo-edit
-      pkgs.cargo-expand
-      pkgs.cargo-feature
-      pkgs.cargo-generate
-      pkgs.cargo-leptos
-      pkgs.cargo-make
-      pkgs.cargo-outdated
-      pkgs.cargo-update
-      pkgs.cargo-watch
-      pkgs.cmake
-      pkgs.curl
-      pkgs.delta
-      pkgs.edgedb
-      pkgs.eza
-      pkgs.fd
-      pkgs.ffmpeg_7
-      pkgs.findutils
-      pkgs.fishPlugins.colored-man-pages
-      pkgs.gawk
-      pkgs.gh
-      pkgs.git
-      pkgs.glow
-      pkgs.gnused
-      pkgs.google-cloud-sdk
-      pkgs.jujutsu
-      pkgs.leptosfmt
-      pkgs.luajitPackages.luarocks
-      pkgs.mas
-      pkgs.neovim
-      pkgs.netlify-cli
-      pkgs.nixd
-      pkgs.nixfmt-rfc-style
-      pkgs.nodePackages.svgo
-      pkgs.nodejs_22
-      pkgs.ocrmypdf
-      pkgs.pandoc
-      pkgs.parallel
-      pkgs.pipe-rename
-      pkgs.pnpm
-      pkgs.podman
-      pkgs.pom
-      pkgs.postgresql_16
-      pkgs.prettierd
-      pkgs.python3
-      pkgs.qmk
-      pkgs.ripgrep
-      pkgs.ripgrep-all
-      pkgs.rustup
-      pkgs.sqlx-cli
-      pkgs.stylua
-      pkgs.taplo
-      pkgs.tealdeer
-      pkgs.timer
-      pkgs.tmuxp # docs: https://tmuxp.git-pull.com/
-      pkgs.tokei
-      pkgs.trash-cli
-      pkgs.typst
-      pkgs.typstfmt
-      pkgs.uutils-coreutils
-      pkgs.wasm-pack
-      pkgs.zoxide
+      # todo: migrate language servers to here from mason
+      # pkgs.rust-analyzer
+      # ✓ basedpyright
+      # ✓ clangd
+      # ✓ codelldb
+      # ✓ css-lsp cssls
+      # ✓ html-lsp html
+      # ✓ lua-language-server lua_ls
+      # ✓ marksman
+      # ✓ sqlfmt
+      # ✓ svelte-language-server svelte
+      # ✓ tailwindcss-language-server tailwindcss
+      # ✓ taplo
+      # ✓ typescript-language-server tsserver
+      # ✓ typst-lsp typst_lsp
+      # ✓ vtsls
+      asciidoctor
+      awscli2
+      bat
+      bfg-repo-cleaner
+      bottom
+      broot # docs: https://dystroy.org/broot/
+      cargo-binstall
+      cargo-edit
+      cargo-expand
+      cargo-feature
+      cargo-generate
+      cargo-leptos
+      cargo-make
+      cargo-outdated
+      cargo-update
+      cargo-watch
+      cmake
+      curl
+      delta
+      edgedb
+      eza
+      fd
+      ffmpeg_7
+      findutils
+      fishPlugins.colored-man-pages
+      gawk
+      gh
+      git
+      glow
+      gnused
+      google-cloud-sdk
+      jujutsu
+      leptosfmt
+      luajitPackages.luarocks
+      mas
+      neovim
+      netlify-cli
+      nixd
+      nixfmt-rfc-style
+      nodePackages.svgo
+      nodejs_22
+      ocrmypdf
+      pandoc
+      parallel
+      pipe-rename
+      pnpm
+      podman
+      pom
+      postgresql_16
+      prettierd
+      python3
+      qmk
+      ripgrep
+      ripgrep-all
+      rustup
+      sqlx-cli
+      stylua
+      taplo
+      tealdeer
+      timer
+      tmuxp # docs: https://tmuxp.git-pull.com/
+      tokei
+      trash-cli
+      typst
+      typstfmt
+      uutils-coreutils
+      wasm-pack
+      yazi
+      zoxide
     ];
 
     variables = {
@@ -84,10 +101,9 @@
     };
   };
 
-  fonts.packages = [
-    pkgs.barlow
-    pkgs.nerdfonts
-    pkgs.open-sans
+  fonts.packages = with pkgs; [
+    google-fonts
+    nerdfonts
   ];
 
 }
