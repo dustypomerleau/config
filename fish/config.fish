7c6863f7c6f97d21e97843315419cdba2da91820
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-11-07 17:18:55 +1100

    feat(fish): add binding for `tmux attach -t`

--- fish/config.fish
+++ fish/config.fish
@@ -217,9 +217,9 @@ pnpm update -g \
 
 abbr --add t "tmux"
 abbr --add ta "tmux attach"
+abbr --add tat "tmux attach -t"
 abbr --add td "tldr"
 abbr --add tl "tmux list-sessions" # use `<c-a> w` for an interactive version of this
-# abbr --add tm "tmpl" # removed after switch to tmuxp
 abbr --add tm "tmuxp load session.yaml"
 abbr --add tua "sudo tlmgr update --self --all --reinstall-forcibly-removed"
 abbr --add x "extract"
