106d1f93a35984cb04d29580b42f78c1d36d0122
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-12-18 18:03:11 +1100

    feat(markdown): repurpose glow binding for markview toggle

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -6,5 +6,13 @@ return {
             "nvim-treesitter/nvim-treesitter",
             "nvim-tree/nvim-web-devicons",
         },
+        keys = {
+            {
+                "<localleader>w",
+                "<cmd>Markview<cr>",
+                mode = { "n" },
+                noremap = true,
+            },
+        },
     },
 }
