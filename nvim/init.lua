0358a265ff813e9ed7b4479715b8c1fb985d24e2
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-01-15 11:25:16 +1100

    chore(sort): sort settings in init

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,10 +14,8 @@ let.lsp_semantic_enabled = 1
 let.netrw_bufsettings = "noma nomod nu nowrap ro nobl"
 
 set.breakindent = true -- keep indent when lines break
-set.completeopt = { "menuone", "noselect", "noinsert" }
-set.splitbelow = true
-set.splitright = true
 set.breakindentopt = "shift:2" -- but shift by 2 spaces
+set.completeopt = { "menuone", "noselect", "noinsert" }
 set.cursorline = true -- highlight the cursor line
 set.expandtab = true -- tabs are spaces
 set.ignorecase = true -- lowercase search is case insensitive
@@ -30,12 +28,14 @@ set.shortmess = vim.opt.shortmess + { c = true } -- don't give completion messag
 set.signcolumn = "yes"
 set.smartcase = true -- uppercase search is case sensitive
 set.softtabstop = 4 -- number of spaces in a tab when editing
+set.splitbelow = true
+set.splitright = true
 set.swapfile = false
 set.synmaxcol = 0 -- highlight lines of any length
 set.tabstop = 4 -- number of visual spaces per tab
 set.termguicolors = true
-set.updatetime = 300
 set.undofile = true
+set.updatetime = 300
 set.wrap = true
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
