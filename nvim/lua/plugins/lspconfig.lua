281612b17966170b5880826f35c3d4013b04751c
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-04-02 10:58:05 +1100

    fix(nvim): change calling of `client:supports_method` to new API

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -84,7 +84,7 @@ return {
                 group = vim.api.nvim_create_augroup("UserLspConfig", {}),
                 callback = function(args)
                     local client = vim.lsp.get_client_by_id(args.data.client_id)
-                    if client.supports_method("textDocument/inlayHint") then
+                    if client:supports_method("textDocument/inlayHint") then
                         vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
                         -- toggle this line to help debug inlay hint problems
                         -- vim.notify(string.format("LSP inlay hints are enabled for %s", client.name), "info")
