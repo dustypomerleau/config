8f0ab5660284c8a49b6a2f6ef287becb19967634
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-06-11 09:41:46 +1000

    chore(colorizer): remove tailwind colors

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -4,14 +4,13 @@ return {
         config = function()
             require("colorizer").setup({
                 user_default_options = {
+                    AARRGGBB = true, -- 0xAARRGGBB hex codes
                     RGB = true, -- #RGB hex codes
                     RRGGBB = true, -- #RRGGBB hex codes
-                    names = false, -- "Name" codes like Blue or blue
                     RRGGBBAA = true, -- #RRGGBBAA hex codes
-                    AARRGGBB = true, -- 0xAARRGGBB hex codes
-                    rgb_fn = true, -- CSS rgb() and rgba() functions
                     hsl_fn = true, -- CSS hsl() and hsla() functions
-                    tailwind = true,
+                    names = false, -- "Name" codes like Blue or blue
+                    rgb_fn = true, -- CSS rgb() and rgba() functions
                 },
             })
         end,
