730ef6723c87bfce2dd314705f2526f29e82e24c
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-06-13 20:43:36 +1000

    feat(snacks): switch to snacks for action picker and indent guides

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -18,27 +18,14 @@ return {
                 local bottom_space = vim.o.cmdheight + (has_statusline and 1 or 0)
                 return { anchor = "SE", col = vim.o.columns, row = vim.o.lines - bottom_space }
             end
-            require("mini.notify").setup({
-                -- sort most recent notifications first
-                sort = function(notif_arr)
-                    table.sort(notif_arr, function(a, b) return a.ts_update > b.ts_update end)
-                    return notif_arr
-                end,
-
-                lsp_progress = { duration_last = 200 },
-                window = { config = win_config, max_width_share = 0.5 },
-            })
-            -- use mini as the default notification system
-            vim.notify = require("mini.notify").make_notify()
 
             -- avoid gx, as this conflicts with gx plugin for following links
             -- keep defaults for evaluate (g=), multiply (gm), replace with register (gr), sort (gs)
-            --
             -- By default this uses `table.sort()`, which is case-sensitive, you can override this
             -- in `setup()` to use `:sort i` if you prefer.
             require("mini.operators").setup({ exchange = { prefix = "ga" } })
 
-            -- using nvim-surround rather than mini.surround due to binding conflicts
+            -- using nvim-surround rather than mini.surround due to binding conflicts with flash in visual mode
         end,
 
         keys = {
--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,5 +1,51 @@
--- todo: replace with https://github.com/folke/snacks.nvim
--- return { { "stevearc/dressing.nvim", event = "VeryLazy" } }
+---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
+local progress = vim.defaulttable()
+
+vim.api.nvim_create_autocmd("LspProgress", {
+    ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+        if not client or type(value) ~= "table" then return end
+        local p = progress[client.id]
+
+        for i = 1, #p + 1 do
+            if i == #p + 1 or p[i].token == ev.data.params.token then
+                p[i] = {
+                    token = ev.data.params.token,
+
+                    msg = ("[%3d%%] %s%s"):format(
+                        value.kind == "end" and 100 or value.percentage or 100,
+                        value.title or "",
+                        value.message and (" **%s**"):format(value.message) or ""
+                    ),
+
+                    done = value.kind == "end",
+                }
+                break
+            end
+        end
+
+        local msg = {} ---@type string[]
+
+        progress[client.id] = vim.tbl_filter(
+            function(v) return table.insert(msg, v.msg) or not v.done end,
+            p
+        )
+
+        local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+
+        vim.notify(table.concat(msg, "\n"), "info", {
+            id = "lsp_progress",
+            title = client.name,
+
+            opts = function(notif)
+                notif.icon = #progress[client.id] == 0 and " "
+                    or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+            end,
+        })
+    end,
+})
 
 return {
     {
@@ -9,13 +55,27 @@ return {
         lazy = false,
 
         opts = {
+            indent = {
+                enabled = true,
+                only_scope = true,
+                priority = 1,
+                hl = { "SnacksIndent" },
+                animate = { enabled = false },
+
+                scope = {
+                    enabled = true,
+                    priority = 200,
+                    hl = { "SnacksIndent1", "SnacksIndent2", "SnacksIndent3" },
+                },
+            },
+
+            input = {},
+            notifier = {}, -- options at https://github.com/folke/snacks.nvim/blob/main/docs/notifier.md
+            picker = {},
+
             bigfile = { enabled = false },
             dashboard = { enabled = false },
             explorer = { enabled = false },
-            indent = { enabled = false },
-            input = { enabled = false },
-            picker = { enabled = false },
-            notifier = { enabled = true }, -- options at https://github.com/folke/snacks.nvim/blob/main/docs/notifier.md
             quickfile = { enabled = false },
             scope = { enabled = false },
             scroll = { enabled = false },
