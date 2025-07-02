ea6057ee5afe5fe9118240c19d5ecd6146798895
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-07-02 18:30:31 +1000

    fix(nvim): back to stable

--- nix-darwin/packages.nix
+++ nix-darwin/packages.nix
@@ -26,7 +26,7 @@ in
     # `environment.systemPackages` are installed for all users (unlike `home.packages`)
     systemPackages = with pkgs; [
       # evcxr # broken panic
-      # neovim
+      # neovim-nightly # using stable
       # unrar # unfree, uncomment this and nixpgks.config.allowUnfree in core.nix if needed
       any-nix-shell # allows fish in nix-shell
       asciidoctor
@@ -78,7 +78,7 @@ in
       luajitPackages.luarocks
       markdown-oxide
       mas
-      neovim-nightly # use stable if you run into treesitter rust issues
+      neovim
       netlify-cli
       nil
       nixfmt-rfc-style
