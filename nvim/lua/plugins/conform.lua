d096980a3ab560459ff9a92aa62ada06306c986f
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-09-13 18:00:20 +1000

    fix(fmt): disable prettier experimental CLI until it respects `tabWidth`

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -11,7 +11,8 @@ return {
                 formatters = {
                     -- The --experimental-cli flag is needed to use @prettier/plugin-oxc.
                     ["dockerfmt"] = { prepend_args = { "--indent=4" } },
-                    ["prettier"] = { prepend_args = { "--experimental-cli" } },
+                    -- adding the flag --experimental-cli is breaking your custom prettier `tabWidth`
+                    -- ["prettier"] = { prepend_args = { "--experimental-cli" } },
                     ["prettypst"] = {
                         prepend_args = { "--use-configuration", "--use-std-in", "--use-std-out" },
                     },
