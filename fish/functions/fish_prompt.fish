608536c6cf38eabd3b8a04f322778d41b6afcd6d
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2026-01-06 15:34:23 +1100

    feat(fish): use warning color for prompt pwd

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -4,7 +4,7 @@ function fish_prompt
     set_color 7e818b
     echo -n (date '+%Y-%m-%d ') # -n suppresses newline
     echo -n (date '+%H:%M:%S ')
-    set_color purple
+    set_color f5c68f
     printf '%s%s' (prompt_pwd) (fish_git_prompt)
     set_color ff4ee0
     printf '] '
