baaa954a2128ad73a153344352e0fbfe45cc63d8
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2026-02-25 14:17:38 +1100

    fix(nix): temporarily disable failed python builds

--- nix-darwin/packages.nix
+++ nix-darwin/packages.nix
@@ -101,7 +101,7 @@
       nodejs_24
       nomino
       nurl
-      ocrmypdf
+      # ocrmypdf # build failure
       opentofu
       ouch
       pandoc
@@ -134,7 +134,7 @@
       timer
       tinymist
       tmux
-      tmuxp # docs: https://tmuxp.git-pull.com/
+      # tmuxp # docs: https://tmuxp.git-pull.com/
       tofu-ls
       tokei
       trash-cli # trash goes to ~/.local/share/Trash/
@@ -151,7 +151,7 @@
       vtsls
       wasm-pack
       yamlfmt
-      yt-dlp
+      # yt-dlp
       yazi
       zellij
       zoxide
