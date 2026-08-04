391d1da3026c4cec71291d0afd3089bb8e34d377
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2026-08-04 17:50:03 +1000

    fix(nvim): initialize treesitter for supported langs using autocmd

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -18,42 +18,106 @@ return {
         build = ":TSUpdate",
 
         config = function()
-            require("nvim-treesitter").install({
+            local langs = {
+                "astro",
                 "bash",
                 "c",
+                "c_sharp",
+                "clojure",
+                "cmake",
+                "cooklang",
+                "cpp",
                 "css",
+                "csv",
+                "dart",
+                "diff",
                 "dockerfile",
                 "editorconfig",
+                "eex",
+                "elixir",
+                "erlang",
+                "fennel",
                 "fish",
                 "git_config",
                 "gitignore",
+                "go",
+                "graphql",
+                "hack",
+                "haskell",
+                "heex",
                 "hjson",
                 "html",
+                "idris",
+                "java",
                 "javascript",
+                "jsdoc",
                 "json",
-                -- "jsonc", -- ?unsupported by update
+                "json5",
+                "jsx",
+                "julia",
+                "kdl",
+                "kotlin",
                 "latex",
                 "lua",
+                "luadoc",
+                "luap",
+                "luau",
                 "markdown",
                 "markdown_inline",
                 "nix",
+                "ocaml",
+                "perl",
+                "php",
+                "pony",
                 "python",
+                "r",
+                "racket",
+                "regex",
+                "rescript",
+                "ruby",
                 "rust",
+                "scala",
+                "scheme",
+                "scss",
                 "sql",
+                "ssh_config",
                 "svelte",
+                "swift",
+                "teal",
                 "terraform",
-                -- "tmux", -- ?unsupported by update
                 "toml",
+                "tsx",
                 "typescript",
                 "typst",
+                "vala",
                 "vim",
                 "vimdoc",
+                "vue",
                 "xml",
                 "yaml",
-            })
+                "zig",
+                "zsh",
+            }
 
+            require("nvim-treesitter").install(langs)
+
+            local remapped = { "jsonc", "plist", "tmTheme" }
+
+            vim.treesitter.language.register("json", "jsonc")
             vim.treesitter.language.register("xml", "plist")
             vim.treesitter.language.register("xml", "tmTheme")
+
+            vim.api.nvim_create_autocmd("FileType", {
+                group = vim.api.nvim_create_augroup("dn_treesitter", { clear = true }),
+
+                callback = function(ev)
+                    if
+                        vim.tbl_contains(langs, ev.match) or vim.tbl_contains(remapped, ev.match)
+                    then
+                        vim.treesitter.start(ev.buf)
+                    end
+                end,
+            })
         end,
     },
 }
